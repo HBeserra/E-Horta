9dbbf1f8c1d8842356092c3889e2e8a6f55ea834
Herbert Feliciano Beserra <Herbert.beserra@gmail.com>
2018-01-29 18:09:57 -0200

    comfiguração do sql local

--- Web/web/conexao.php
+++ Web/web/conexao.php
@@ -1,8 +1,8 @@
 <?php
-	$servidor = "mysql.hostinger.com.br";
-	$usuario = "u294779963_horta";
-	$senha = "abc123";
-	$dbname = "u294779963_horta";
+	$servidor = "localhost";
+	$usuario = "root";
+	$senha = "";
+	$dbname = "horta";
 	
 	//Criar a conexao
 	$conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
