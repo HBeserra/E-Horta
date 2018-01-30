be52bf6c1912a35dbb8a1ac96a8ff8ba8c5d7ba6
Herbert Feliciano Beserra <Herbert.beserra@gmail.com>
2018-01-30 15:14:22 -0200

    sistema de validação do cadrastro

--- Web/web/cadastrar.php
+++ Web/web/cadastrar.php
@@ -4,22 +4,68 @@ ob_start();
 $btnCadUsuario = filter_input(INPUT_POST, 'btnCadUsuario', FILTER_SANITIZE_STRING);
 if($btnCadUsuario){
 	include_once 'conexao.php';
+	$dados_rc = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 	
-    $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
-	var_dump($dados);
+	$erro = false;
 	
-    if(($dados['senha']) == ($dados['senha_confirmação'])){
+	$dados_st = array_map('strip_tags', $dados_rc);
+	$dados = array_map('trim', $dados_st);
+	
+	if(in_array('',$dados)){
+		$erro = true;
+		$_SESSION['msg'] = "Necessário preencher todos os campos";
         
         
-    }else{
-        echo $_SESSION['msg'] = "Erro senhas diferentes";
-    }
-    
-    
-}else{
-    $_SESSION['msg'] = "Erro ao cadastrar o usuário";
+	}elseif((strlen($dados['senha'])) != (strlen($dados['senha_confirmação']))){
+		$erro = true;
+		$_SESSION['msg'] = "As senhas devem ser iguais";
+
+    }elseif((strlen($dados['senha'])) < 6){
+		$erro = true;
+		$_SESSION['msg'] = "A senha deve ter no minímo 6 caracteres";
+	}elseif(stristr($dados['senha'], "'")) {
+		$erro = true;
+		$_SESSION['msg'] = "Caracter ( ' ) utilizado na senha é inválido";
+	}else{
+		$result_usuario = "SELECT id FROM usuarios WHERE usuario='". $dados['usuario'] ."'";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)){
+			$erro = true;
+			$_SESSION['msg'] = "Este usuário já está sendo utilizado";
+		}
+		
+		$result_usuario = "SELECT id FROM usuarios WHERE email='". $dados['email'] ."'";
+		$resultado_usuario = mysqli_query($conn, $result_usuario);
+		if(($resultado_usuario) AND ($resultado_usuario->num_rows != 0)){
+			$erro = true;
+			$_SESSION['msg'] = "Este e-mail já está cadastrado";
+		}
 	}
+	
+	
+	//var_dump($dados);
+	if(!$erro){
+		//var_dump($dados);
+		$dados['senha'] = password_hash($dados['senha'], PASSWORD_DEFAULT);
+		
+		$result_usuario = "INSERT INTO usuarios (nome, email, usuario, senha) VALUES (
+						'" .$dados['nome']. "',
+						'" .$dados['email']. "',
+						'" .$dados['usuario']. "',
+						'" .$dados['senha']. "'
+						)";
+		$resultado_usario = mysqli_query($conn, $result_usuario);
+		if(mysqli_insert_id($conn)){
+			$_SESSION['msgcad'] = "Usuário cadastrado com sucesso";
+			header("Location: login.php");
+		}else{
+			$_SESSION['msg'] = "Erro ao cadastrar o usuário";
+		}
+	}
+	
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
